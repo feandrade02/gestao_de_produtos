502002adca37113a69c34d0e71fd1a6bd5dfd61f
Felipe Andrade <fe.a.andrade24@gmail.com>
2024-09-03 16:15:08 -0300

    novas modificações

--- src/components/pages/Login.jsx
+++ src/components/pages/Login.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
-import { Avatar, Button, CssBaseline, TextField, FormControlLabel, Checkbox, Link, Paper, Box, Grid2, Typography } from '@mui/material';
+import { Button, CssBaseline, TextField, Paper, Box, Grid2, Typography } from '@mui/material';
 import { createTheme, ThemeProvider } from '@mui/material/styles';
+import LoginIcon from '@mui/icons-material/Login';
 
 import Logo from '../../img/logo_teste.png'
 
@@ -19,23 +20,37 @@ export default function Login() {
 
     return(
         <ThemeProvider theme={theme}>
-        <Grid2 container component="main" sx={{ height: '100vh'}}>
+        <Grid2 container component="main" sx={{ height: '100vh', display: 'flex', justifyContent: 'center', alignItems: 'center',}}>
         <CssBaseline />
-        <Grid2 item xs={12} sm={8} md={5} component={Paper} elevation={6} square>
+        <Grid2 
+          item 
+          xs={12} 
+          sm={8} 
+          md={7} 
+          component={Paper} 
+          elevation={6} 
+          square
+          sx={{
+            padding: '20px',
+            maxWidth: '600px',
+            width: '100%',
+          }}
+        >
           <Box
             sx={{
               my: 8,
-              mx: 4,
+              mx: 4, 
               display: 'flex',
               flexDirection: 'column',
               alignItems: 'center',
+              
             }}
           >
             <Box 
                 component='img'
                 sx={{
                     height: 100,
-                    mb: 2,
+                    mb: 2 
                 }}
                 alt='Logomarca'
                 src={Logo}
@@ -43,34 +58,49 @@ export default function Login() {
             <Typography component="h1" variant="h5">
               Catálogo de Produtos
             </Typography>
-            <Box component="form" noValidate onSubmit={handleSubmit} sx={{ mt: 1 }}>
-              <TextField
-                margin="normal"
-                required
-                fullWidth
-                id="email"
-                label="Usuário"
-                name="email"
-                autoComplete="email"
-                autoFocus
-              />
-              <TextField
-                margin="normal"
-                required
-                fullWidth
-                name="password"
-                label="Senha"
-                type="password"
-                id="password"
-                autoComplete="current-password"
-              />
-              <Button
-                type="submit"
-                variant="contained"
-                sx={{ mt: 3, mb: 2 }}
-              >
-                Login
-              </Button>
+            <Box 
+              component="form" 
+              noValidate 
+              onSubmit={handleSubmit} 
+              sx={{ 
+                mt: 1, 
+                width: '100%',
+                display: 'flex',
+                flexDirection: 'column',
+                alignItems: 'center', 
+              }}>
+                <TextField
+                  margin="normal"
+                  required
+                  fullWidth
+                  id="email"
+                  label="Usuário"
+                  name="email"
+                  autoComplete="email"
+                  autoFocus
+                />
+                <TextField
+                  margin="normal"
+                  required
+                  fullWidth
+                  name="password"
+                  label="Senha"
+                  type="password"
+                  id="password"
+                  autoComplete="current-password"
+                />
+                <Button
+                  type="submit"
+                  variant="contained"
+                  startIcon={<LoginIcon />}
+                  sx={{
+                    mt: 3, 
+                    mb: 2,
+                    mx: 'auto' 
+                  }}
+                >
+                  Login
+                </Button>
             </Box>
           </Box>
         </Grid2>
