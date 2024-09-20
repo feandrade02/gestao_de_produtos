3f58f72435504a79a3fb6496f6b2c8929a10e052
feandrade02 <fe.a.andrade@hotmail.com>
2024-09-20 00:32:07 -0300

    att

--- src/components/pages/GestaoCampos.jsx
+++ src/components/pages/GestaoCampos.jsx
@@ -15,25 +15,23 @@ export default function GestaoCampos() {
 
     return (
         <PaginaBase>
-            <Container>
-                <Box>
-                    <Tabs 
-                        value={activeTab}
-                        onChange={handleChange}
-                        aria-label="tabs example"
-                        style={{marginTop: "1.5rem", marginBottom: "3rem"}}
-                    >
-                        <Tab label="Serviço" />
-                        <Tab label="Ação" />
-                        <Tab label="Status da Ação" />
-                    </Tabs>
-                    <Box className="tab-content">
-                        {activeTab === 0 && <Servico />}
-                        {activeTab === 1 && <Acao />}
-                        {activeTab === 2 && <StatusAcao />}
-                    </Box>
+            <Box sx={{ml: '1.5rem'}}>
+                <Tabs 
+                    value={activeTab}
+                    onChange={handleChange}
+                    aria-label="tabs example"
+                    style={{marginTop: "1.5rem", marginBottom: "3rem"}}
+                >
+                    <Tab label="Serviço" />
+                    <Tab label="Ação" />
+                    <Tab label="Status da Ação" />
+                </Tabs>
+                <Box className="tab-content">
+                    {activeTab === 0 && <Servico />}
+                    {activeTab === 1 && <Acao />}
+                    {activeTab === 2 && <StatusAcao />}
                 </Box>
-            </Container>
+            </Box>
         </PaginaBase>
     );
 }
